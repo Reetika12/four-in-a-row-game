5874e4e9a4ab9c260967ec43c62b1bbef6d74e52
reetika <reetika.singh@byjus.com>
2021-04-16 22:15:25 +0530

    first commit of react app set up

---
+++ src/Container/MainPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class MainPage extends Component {
+
+    render() {
+        return (
+            <div>
+                
+                <img src={require('../Image/4inarow.png').default} alt="" />
+            </div>
+        )
+    }
+}
+
+export default MainPage
