2998cff99a5444313b2107816132647d97693903
reetika <reetika.singh@byjus.com>
2021-04-18 17:22:09 +0530

    final ui for main page implemented

--- src/Container/MainPage.js
+++ src/Container/MainPage.js
@@ -20,14 +20,14 @@ const CustomRadio = withStyles({
 })((props) => <Radio color="default" {...props} />);
 
 class MainPage extends Component {
-     constructor(props) {
-      super(props);
-       this.state={
-           dialogOpen: false,
-           page:'main',
-           selectedValue:''
-       }
-     }
+    constructor(props) {
+        super(props);
+        this.state = {
+            dialogOpen: false,
+            page: 'main',
+            selectedValue: ''
+        }
+    }
     handleDialogOpen = () => {
         this.setState({ dialogOpen: true });
     };
@@ -35,18 +35,18 @@ class MainPage extends Component {
     handleClose = () => {
         this.setState({ dialogOpen: false });
     };
-     
+
     handleChangeForGame = (event) => {
         this.setState({
             selectedValue: event.target.value,
         });
     };
 
-   onClickEvent = () =>{
-     this.setState({
-         dialogOpen:true
-     })
-   }
+    onClickEvent = () => {
+        this.setState({
+            dialogOpen: true
+        })
+    }
 
     render() {
         return (
@@ -59,40 +59,44 @@ class MainPage extends Component {
                         <div className="connectFourText">Connect Four!</div>
                         <div className="connectFourBelowText">Play with other players around the world.</div>
                     </div>
-                    <div className="parentsquare">
-                        <div className="playButtonStyle">
-                            <img src={require('../Image/play-3-32.png').default} alt="" />
-                            <div className="playText">
-                                PLAY
+                    <div className="footerStyle">
+                        <div className="parentsquare">
+                            <div className="playButtonStyle">
+                                <img src={require('../Image/play-3-32.png').default} alt="" />
+                                <div className="playText">
+                                    PLAY
                         </div>
-                     </div>
-                        <div style={{ display: 'flex', flexDirection: 'column', alignSelf: 'flex-end',marginRight:'100px'}}>
-                           <div style={{display:'flex',flexDirection:'row', padding:'10px 10px'}}>
-                                <Button
-                                    src={require('../Image/one.png').default}
-                                    buttonText="Custom Game"
-                                    type="customgame"
-                                    clickEvent={this.onClickEvent}
-                                />
-                                <Button
-                                    src={require('../Image/two.png').default}
-                                    buttonText="Two Players"
-                                    type="twoplayers"
-                                />
                             </div>
-                            <div style={{ display: 'flex', flexDirection: 'row', padding: '10px 10px' }}>
-                                <Button
-                                    src={require('../Image/online.png').default}
-                                    buttonText="Game Online"
-                                    type="gameonline"
-                                />
-                                <Button
-                                    src={require('../Image/training.png').default}
-                                    buttonText="Training Game"
-                                    type="traininggame"
-                                />
+                            <div className="underlinemainCard"></div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <div style={{ display: 'flex', flexDirection: 'row', padding: '10px 10px' }}>
+                                    <Button
+                                        src={require('../Image/one.png').default}
+                                        buttonText="Custom Game"
+                                        type="customgame"
+                                        clickEvent={this.onClickEvent}
+                                    />
+                                    <Button
+                                        src={require('../Image/two.png').default}
+                                        buttonText="Two Players"
+                                        type="twoplayers"
+                                    />
+                                </div>
+                                <div style={{ display: 'flex', flexDirection: 'row', padding: '10px 10px' }}>
+                                    <Button
+                                        src={require('../Image/online.png').default}
+                                        buttonText="Game Online"
+                                        type="gameonline"
+                                    />
+                                    <Button
+                                        src={require('../Image/training.png').default}
+                                        buttonText="Training Game"
+                                        type="traininggame"
+                                    />
+                                </div>
                             </div>
                         </div>
+                        <div className="footermain">© 2020</div>
                     </div>
                 </div>
                 <Dialog
@@ -100,26 +104,26 @@ class MainPage extends Component {
                     onClose={this.handleClose}
                     aria-labelledby="max-width-dialog-title"
                 >
-                <div style={{display:'flex',flexDirection:'column',padding:'30px 30px'}}>
-                    <div className="numberOfGame">Number of game</div>
-                    <RadioGroup aria-label="gender" name="gender1" value={this.state.selectedValue} onChange={this.handleChangeForGame}>
-                        <FormControlLabel className="singleBox" style={{marginLeft:'0px',marginRight:'0px'}} value="games2" control={<CustomRadio />} label="2 Games" />
-                        <FormControlLabel className="singleBox" style={{ marginLeft: '0px', marginRight: '0px' }} value="games3" control={<CustomRadio />} label="3 Games" />
-                        <FormControlLabel className="singleBox" style={{ marginLeft: '0px', marginRight: '0px' }}  value="games5" control={<CustomRadio />} label="5 Games" />
-                        <FormControlLabel className="singleBox" style={{ marginLeft: '0px', marginRight: '0px' }} value="games10" control={<CustomRadio />} label="10 Games" />
-                    </RadioGroup>
-                    <div className="underlineStyle"></div>
-                    <div style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
-                        <Button
-                            buttonText="CANCEL"
-                            type="cancel"
-                        />
-                        <Button
-                            buttonText="OK"
-                            type="OK"
-                        />
+                    <div style={{ display: 'flex', flexDirection: 'column', padding: '30px 30px' }}>
+                        <div className="numberOfGame">Number of game</div>
+                        <RadioGroup aria-label="gender" name="gender1" value={this.state.selectedValue} onChange={this.handleChangeForGame}>
+                            <FormControlLabel className="singleBox"  value="games2" control={<CustomRadio />} label="2 Games" />
+                            <FormControlLabel className="singleBox"  value="games3" control={<CustomRadio />} label="3 Games" />
+                            <FormControlLabel className="singleBox"  value="games5" control={<CustomRadio />} label="5 Games" />
+                            <FormControlLabel className="singleBox"  value="games10" control={<CustomRadio />} label="10 Games" />
+                        </RadioGroup>
+                        <div className="underlineStyle"></div>
+                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                            <Button
+                                buttonText="CANCEL"
+                                type="cancel"
+                            />
+                            <Button
+                                buttonText="OK"
+                                type="OK"
+                            />
+                        </div>
                     </div>
-                </div>  
                 </Dialog>
             </div>
         )
